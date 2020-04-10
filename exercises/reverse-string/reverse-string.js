21e99606cf759a73d4cee38b6b2beaa6b53f938a
charlotte adams <lucky.seattle.monkey@gmail.com>
2020-04-09 22:34:08 -0700

    write reverse string function with built-in methods & as a for-loop for interview practice

--- exercises/reverse-string/reverse-string.js
+++ exercises/reverse-string/reverse-string.js
@@ -7,8 +7,22 @@ export const reverseString = (string) => {
     if (string === '') {
        return string;
       }
-  
+  // This is how to reverse a string with js methods. 
+      // const newString = string
+      //   .split('')
+      //   .reverse() => reverse can be called on an array
+      //   .join('');
+
+      // This is how to reverse a string for an interview! :)
+      let newString = '';
+      for(let i = string.length -1; i >= 0; i --) {
+        newString += string[i];
+      }
     
+                          
+      return newString;
 };
   
+
+
 
